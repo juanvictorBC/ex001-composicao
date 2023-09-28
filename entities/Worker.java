6a6a62b27454f474ac108a550f21592be27a496c
juanvictorBC <juanvictor_bc@hotmail.com>
2023-09-28 19:42:57 -0300

    conclusao

--- entities/Worker.java
+++ entities/Worker.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -10,58 +11,78 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	
-	//composição de objetos
+
+	// composição de objetos
 	private Department department;
 	private List<HourContact> contracts = new ArrayList<>();
-	
+
 	public Worker() {
 	}
-	//quando tiver uma lista nos atributos, nao gerar construtor pra ela.
+
+	// quando tiver uma lista nos atributos, nao gerar construtor pra ela.
 	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
-		this.department = department;		
+		this.department = department;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public WorkerLevel getLevel() {
 		return level;
 	}
+
 	public void setLevel(WorkerLevel level) {
 		this.level = level;
 	}
+
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
+
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+
 	public Department getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
+
 	public List<HourContact> getContracts() {
 		return contracts;
 	}
-	
-	public void addContract (HourContact contract) {
+
+	public void addContract(HourContact contract) {
 		contracts.add(contract);
 	}
-	
+
 	public void removeContract(HourContact contract) {
 		contracts.remove(contract);
 	}
-	
-	
-	
-	
-	
+
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContact c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+
 }
